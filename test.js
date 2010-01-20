5187336432b1ab35fe599c5c5c8dd3f5e5ced827
Joshua Holt <holt.josh@gmail.com>
2010-01-19 23:15:42 -0500

    Add a few more tests before implementing more method

--- test.js
+++ test.js
@@ -7,7 +7,8 @@ var redis = new redisclient.Client();
 
 // Used in tests
 var nodeq = "resque:queue:nodeq";
-var nodeqJob = {message: "This is a test queue item", actor: "Mailer"};
+var nodeqJob = {message: "This is test queue item(1)", actor: "Mailer"};
+var nodeqJob1 = {message: "This is test queue item(2)", actor: "Mailer"};
 
 redis.flushdb().wait();
 
@@ -21,17 +22,36 @@ describe("When Manipulating a Queue")
   it("should allow jobs to be pushed onto the queue", function(){
     assert.equal(true,queue.push(nodeq,nodeqJob).wait());
   })
+  it("should allow jobs to be popped off of the queue",function() {
+    assert.equal(JSON.stringify(nodeqJob),queue.pop(nodeq).wait());
+    assert.equal(true,queue.push(nodeq,nodeqJob).wait());
+  })
   it("should allow range peeking",function() {
     assert.equal(JSON.stringify(nodeqJob),queue.peek(nodeq,0,-1).wait()); 
   })
   it("should have one item after pushing job onto queue",function() {
     assert.equal(1,queue.size(nodeq).wait());
   })
+  it("should allow multiple jobs to be pushed onto the queue", function(){
+    assert.equal(true,queue.push(nodeq,nodeqJob).wait());
+    assert.equal(true,queue.push(nodeq,nodeqJob1).wait());
+  })
+  it("should have three jobs after two more pushes",function() {
+    assert.equal(3,queue.size(nodeq).wait());
+  })
+  it("should have two jobs after a pop",function() {
+    assert.equal(JSON.stringify(nodeqJob),queue.pop(nodeq).wait());
+    assert.equal(2,queue.size(nodeq).wait());
+  })
   it("should allow range fetching",function() {
     assert.equal(JSON.stringify(nodeqJob),queue.list_range(nodeq,0,-1).wait()); 
   })
-  it("should allow jobs to be popped off of the queue",function() {
+  it("should have one job left after pop", function() {
     assert.equal(JSON.stringify(nodeqJob),queue.pop(nodeq).wait());
+    assert.equal(1,queue.size(nodeq).wait());
+  })
+  it("should allow different jobs to be in the queue",function() {
+    assert.equal(JSON.stringify(nodeqJob1),queue.pop(nodeq).wait());
   })
   it("should have a size of zero after popping the last item off of the queue",function() {
     assert.equal(0,queue.size(nodeq).wait());

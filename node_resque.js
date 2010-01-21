4252233e1409f764186ae3838ac1048484d56e77
Joshua Holt <holt.josh@gmail.com>
2010-01-21 11:54:19 -0500

    Implemented the remaining API methods execpt "info"

--- node_resque.js
+++ node_resque.js
@@ -12,6 +12,7 @@ var tcp = require('tcp');
 var redis = require('./lib/redisclient');
 var helpers = require('./lib/helpers');
 var client = new redis.Client();
+require('underscore');
 
 exports.create = function(db, ns) {
   return new NodeResque(db, ns);
@@ -112,7 +113,18 @@ NodeResque.prototype.queues = function() {
 };
 
 NodeResque.prototype.remove_queue = function(queue) {
-  
+  var that = this, promise = new process.Promise();
+  this.emit('remove_queue', queue);
+  client.srem(this.ns+":queues",queue).addCallback(function(ret) {
+    client.del(this.ns+":queue:"+queue).addCallback(function(ret) {
+      promise.emitSuccess(ret);
+    }).addErrback(function(e) {
+      promise.addErrback(e);
+    })
+  }).addErrback(function(e) {
+    promise.emitError(e);
+  });
+  return promise;
 };
 
 NodeResque.prototype.watch_queue = function(queue) {
@@ -132,31 +144,47 @@ NodeResque.prototype.watch_queue = function(queue) {
 };
 
 NodeResque.prototype.enqueue = function(klass, args) {
-  
+  var queue = klass.queue;
+  if (queue) {
+    return Job.create(queue,klass,args);
+  }
 };
 
 NodeResque.prototype.reserve = function(queue) {
-  
+  return Job.reserve(queue);
 };
 
 NodeResque.prototype.workers = function() {
-  
+  return Worker.all;
 };
 
 NodeResque.prototype.working = function() {
-  
+  return Worker.working
 };
 
 NodeResque.prototype.info = function() {
-  
+  // return {
+  //   pending: this.queues.addCallback(function(ret) {
+  //     _.reduce(ret, 0, function(m,k){ return m + this.size(k).wait() });
+  //   }).addErrback(function(e) {
+  //     promise.emitError(e);
+  //   }),
+  //   processed: Stat[:processed],
+  //   queues:    queues.size,
+  //   workers:   workers.size,
+  //   working:   working.size,
+  //   failed:    Stat[:failed],
+  //   servers:   [redis.server]
+  // }
 };
 
 NodeResque.prototype.keys = function() {
-  
-};
-
-NodeResque.prototype.runner = function() {
-  while(this.callbacks.length > 0) {
-    this.callbacks.shift()();
-  }
+  var that = this,promise = new process.Promise();
+  this.emit('keys');
+  client.keys('*').addCallback(function(ret) {
+    promise.emitSuccess(_.map(ret,function(v){return v.replace('resque:','')}));
+  }).addErrback(function(e) {
+    promise.emitError(e);
+  });
+  return promise;
 };
